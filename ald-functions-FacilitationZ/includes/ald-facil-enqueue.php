6e5f4991e9cf1662cb627c70f504ae73286208a6
Anne-Laure Delpech <contact@parcours-performance.com>
2017-03-02 09:27:12 +0100

    ajustements styles formulaire des landing pages

--- ald-functions-FacilitationZ/includes/ald-facil-enqueue.php
+++ ald-functions-FacilitationZ/includes/ald-facil-enqueue.php
@@ -24,22 +24,14 @@ if (! function_exists('ald_facil_enqueue_scripts') ){
 		'all' // no media type
 	);
 
-	wp_register_style(
-		'font_awesome_css',
-		ALD_FACIL_URL . 'css/font-awesome.css',
-		array(),
-		null,
-		'all' // no media type
-	);
-	
-	
+
 	wp_enqueue_style( 'ald_facil_style' ) ;
-	wp_enqueue_style( 'font_awesome_css' ) ;
+
 	
 	// enqueue fonts
 	wp_enqueue_style( 
 		'google-nova-round', 
-		'http://fonts.googleapis.com/css?family=Nova+Round'
+		'https://fonts.googleapis.com/css?family=Nova+Round'
 	);
 	
 	// enqueue scripts
